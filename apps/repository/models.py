e2f2c24be2d58dfc59f0f802c956c4dedb949212
mskim5383 <mskim5383@gmail.com>
2016-01-21 00:57:14 +0900

    repository/models 구현 (incomplete)

--- apps/repository/models.py
+++ apps/repository/models.py
@@ -1,5 +1,41 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from apps.session.models import UserProfile
 
 # Create your models here.
+
+
+
+class Repository(models.Model):
+    userprofile = models.ForeignKey(UserProfile,
+                                    null=False, related_name='repository')
+    master_branch = models.OneToOneField(Branch,
+                                    null=False)
+    readme = models.TextField(default='', max_length=1000)
+
+
+class Branch(models.Model):
+    repository = models.ForeignKey(Repository,
+                                null=False,  related_name='branch')
+    head_commit = models.OneToOneField(Commit,
+                                null=False, related_name='branch')
+
+
+class Commit(models.Model):
+    branch = models.ForeignKey(Branch,
+                                null=False, related_name='commit')
+
+
+class Movement(models.Model):
+    commit = models.ForeignKey(Commit,
+                                null=False, related_name='movement')
+    meta_data = models.TextField(default='', max_length=200)
+    no = models.IntegerField(default=-1)
+
+
+class Part(models.Model):
+    order = models.IntegerField(default=-1)
+    meta_data = models.TextField(default='', max_length=200)
+    notes = models.TextField(default='', max_length=10000)
+
