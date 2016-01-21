bb11879a9d8cf5d81960445d5d392662fb955721
mskim5383 <mskim5383@gmail.com>
2016-01-21 16:47:03 +0900

    repository/models 구현

--- apps/repository/models.py
+++ apps/repository/models.py
@@ -1,34 +1,33 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from apps.session.models import UserProfile
 
 # Create your models here.
 
 
 
 class Repository(models.Model):
-    userprofile = models.ForeignKey(UserProfile,
+    userprofile = models.ForeignKey('session.UserProfile',
                                     null=False, related_name='repository')
-    master_branch = models.OneToOneField(Branch,
-                                    null=False)
+    master_branch = models.OneToOneField('Branch',
+                                    null=False, related_name='_repository')
     readme = models.TextField(default='', max_length=1000)
 
 
 class Branch(models.Model):
-    repository = models.ForeignKey(Repository,
+    repository = models.ForeignKey('Repository',
                                 null=False,  related_name='branch')
-    head_commit = models.OneToOneField(Commit,
-                                null=False, related_name='branch')
+    head_commit = models.OneToOneField('Commit',
+                                null=False, related_name='_branch')
 
 
 class Commit(models.Model):
-    branch = models.ForeignKey(Branch,
+    branch = models.ForeignKey('Branch',
                                 null=False, related_name='commit')
 
 
 class Movement(models.Model):
-    commit = models.ForeignKey(Commit,
+    commit = models.ForeignKey('Commit',
                                 null=False, related_name='movement')
     meta_data = models.TextField(default='', max_length=200)
     no = models.IntegerField(default=-1)
