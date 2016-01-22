ce39631d98dc2233a2db19473a077dbba57fdb58
mskim5383 <mskim5383@gmail.com>
2016-01-22 21:31:08 +0900

    dark-theme 로직 개선

--- apps/main/views.py
+++ apps/main/views.py
@@ -5,4 +5,7 @@ from django.shortcuts import render
 
 
 def main_page(request):
-    return render(request, 'main/main_page.html', {})
+    dark_theme = True
+    if request.user.is_authenticated():
+        dark_theme = False
+    return render(request, 'main/main_page.html', {'dark_theme': dark_theme})
