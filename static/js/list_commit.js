66772b6d99b4c753672933d78c7d893ea170ce0a
Jaemin_Shin <999fg@kaist.ac.kr>
2016-01-27 03:54:57 +0900

    branch_base completed

---
+++ static/js/list_commit.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+   $("#star").on("click", function() {
+        var star = $(this);
+        $.ajax(
+        {
+            url : "../../star/",
+            type : "POST",
+            data : {csrfmiddlewaretoken:$("input[name=csrfmiddlewaretoken]").val()},
+            dataType : "json",
+            success : function(data){
+                console.log(data.status);
+                if(data.status == "star"){
+                    star.attr("class", "star");
+                    star.html("Unstar");
+
+                }
+                else if(data.status == "unstar"){
+                    star.attr("class", "unstar");
+                    star.html("Star");
+                }
+            }
+        });
+    });
+   $("#abc").val($(".score-select").attr("value"));
+   abc_editor = new ABCJS.Editor("abc", {paper_id: "paper0", warnings_is: "warnings"});
+   
+   $(".part-select, .score-select").on("click", function() {
+       $("#abc").val($(this).attr("value"));
+       $(this).parent().parent().children(".active").attr("class", "");
+       $(this).parent().attr("class", "active");
+   });
+});
